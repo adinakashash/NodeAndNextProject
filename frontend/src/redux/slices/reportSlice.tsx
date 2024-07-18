7e1c20ac7ec13761768e467ee4dfc5e347f22220
efratburshtein <efrat1574@gmail.com>
2024-07-18 03:07:25 +0300

    worker and user in react

--- frontend/src/redux/slices/reportSlice.tsx
+++ frontend/src/redux/slices/reportSlice.tsx
@@ -1,24 +1,24 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { RootState } from "../store";
 import axios from "axios";
-import { Location, ReportClass } from "@/classes/report";
+import {  ReportClass } from "@/classes/report";
 import { useDispatch } from "react-redux";
 const http = "http://localhost:3000";
-const { data = {} } = [];
+
+const initialState = {
+  reports: [] 
+};
 
 const reportSlice = createSlice({
-  name: "reports",
-  initialState: data,
+  name: 'reports',
+  initialState,
   reducers: {
-    setReports(state, action) {  
-      console.log(action.payload);
-       
+    setReports(state, action) {
       state.reports = action.payload;
-      console.log(state.reports);
-                
-    },
-  },
+    }
+  }
 });
+
 
 export const {} = reportSlice.actions;
 export const selectReports = (state: RootState) => state.reports;
@@ -27,7 +27,7 @@ export default reportSlice.reducer;
 export const getReportByCity = createAsyncThunk('', async (city:string,{ dispatch }) => {
   try {    
     const response = await axios.get(`${http}/reports/${city}`);
-    const data= response.data;
+    const data= response.data;    
     dispatch(reportSlice.actions.setReports(data));      
     return data;
   } catch (error) {
